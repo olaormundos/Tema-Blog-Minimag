0cb6e2a407b145e5a30348a75db809db45eefe09
olaormundos <vanderleidasilvaormundo@gmail.com>
2023-02-28 22:37:23 -0300

    comentários

--- include/customizer/theme-customizer.php
+++ include/customizer/theme-customizer.php
@@ -3,6 +3,14 @@ require get_template_directory().'/include/customizer/social.php';
 require get_template_directory().'/include/customizer/layout.php';
 
 function olm_customize_register( $wp_customize ) {
+  // altera section existentes por padrão do Wordpress
+  $wp_customize->get_section('custom_css')->description = '';
+  // adiciona um painel com as opções do tema no customizer
+  $wp_customize->add_panel('opcoes', array(
+    'title'     => 'Opções do Tema',
+    'priority'  => 1
+  ));
+  // registra as opções de customização no tema
   olm_social_customizer( $wp_customize );
   olm_layout_customizer( $wp_customize );
 }
